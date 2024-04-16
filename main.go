111063edbdc29da79c9e2fe39e6be364fe22eba1
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-16 23:54:34 +0100

    connection to the database

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
 func main() {
+
+	db, err := sql.Open("mysql", "root:eunice99x@tcp(127.0.0.1:3306)/platform_engineer_clone")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
